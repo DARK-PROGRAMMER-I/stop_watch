338871a468e8c1362a0d8f9cf85474309acd5a25
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2022-02-16 22:29:01 +0500

    Stop watch app from book " Flutter Cook Book" * Buttons Added

--- lib/main_classes/stopwatch.dart
+++ lib/main_classes/stopwatch.dart
@@ -10,16 +10,27 @@ class StopWatch extends StatefulWidget{
 
 class StopWatchState extends State<StopWatch>{
   // Now we need to create Seconds and timer
-  int? seconds;
+  bool? isStart = false;
+  int? seconds= 0;
   Timer? timer;
 
   // now build functions
-  @override
-  void initState(){
-    super.initState();
+  // Building functions for stoping and Starting the timer
+  void _startTimer(){
+    timer = Timer.periodic(Duration(seconds: 1), _onTick);
 
-    seconds= 0;
-    timer= Timer.periodic(Duration(seconds:1), _onTick);
+    setState(() {
+      seconds= 0;
+      isStart= true;
+    });
+  }
+  // Stop Timer
+  void _stopTimer(){
+    setState((){
+      timer!.cancel();
+      isStart = false;
+
+    });
   }
   // Function for incrementing the number
   void _onTick(Timer time){
@@ -30,12 +41,7 @@ class StopWatchState extends State<StopWatch>{
 
   // Creating dynamic text -> use infront of seconds
   String _secondsText() => seconds == 1? 'second': 'seconds';
-  // create a function to stop Timer when off the screen
-  @override
-  void dispose(){
-    timer?.cancel();
-    super.dispose();
-  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -43,11 +49,43 @@ class StopWatchState extends State<StopWatch>{
         backgroundColor: Color(0xff3eb489),
         title: Text("Stop Watch", style: TextStyle(color: Colors.white),),
       ),
-      body: Container(
-        child: Center(
-          child: Text("$seconds ${_secondsText()}", style: TextStyle(fontSize: 20),),
-        ),
+      body:  Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:[
+          Center(
+            child: Text("$seconds ${_secondsText()}", style: TextStyle(fontSize: 20),),
+          ),
+          SizedBox(height: 15,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: _startTimer,
+                  child: Text("Start", style: TextStyle(fontSize: 16, letterSpacing: 2),),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Color(0xff3eb489)),
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                    padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 30))
+                  ),
+
+              ),
+              SizedBox(width: 20,),
+              TextButton(
+                  onPressed: _stopTimer,
+                  child: Text('Stop'),
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all(Colors.red),
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
+                    padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 30))
+                  ),
+
+              )
+
+            ],
+          )
+      ]
       ),
+
     );
   }
 }
